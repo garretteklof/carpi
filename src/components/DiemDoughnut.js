3b5df8501e22fc6c9bfc528e943fd278306cb983
garretteklof <garrett.eklof@gmail.com>
2018-02-09 20:03:38 -0600

    add >24 hours alert

--- src/components/DiemDoughnut.js
+++ src/components/DiemDoughnut.js
@@ -4,12 +4,12 @@ import Doughnut from 'react-chartjs-2';
 export default class DiemDoughnut extends React.Component {
 
 	nameToArray = (activities, remainder) => {
-		const nameArray = activities.map(({name}) => name);
-		return remainder > 0 ? [...nameArray, 'remainder'] : [...nameArray];
+		const nameToArray = activities.map(({name}) => name);
+		return remainder > 0 ? [...nameToArray, 'remainder'] : [...nameToArray];
 	}
 	timeSpentToArray = (activities, remainder) => {
-		const timeSpentArray = activities.map(({timeSpent}) => timeSpent);
-		return remainder > 0 ? [...timeSpentArray, remainder] : [...timeSpentArray];
+		const timeSpentToArray = activities.map(({timeSpent}) => timeSpent);
+		return remainder > 0 ? [...timeSpentToArray, remainder] : [...timeSpentToArray];
 	}
 
 	setColors = () => {
@@ -18,20 +18,37 @@ export default class DiemDoughnut extends React.Component {
 		const remainderIndex = this.nameToArray(act, rem).lastIndexOf('remainder');
 		const remainderColor = '#CACFD6';
 		const colors = ['#FF6384','#36A2EB','#FFCE56','#b40cb8','#fd6996'];
-		if( remainderIndex !== -1) {
+
+		if ( remainderIndex !== -1) {
 			colors.splice(remainderIndex, 0, remainderColor);
 		}
 		return colors;
 	}
 
-	setData = (label, data) => ({
-		labels: label,
-		datasets: [{
-			data: data,
-			backgroundColor: this.setColors(),
-			hoverBackgroundColor: this.setColors()
-		}]
-	})
+	setData = (label, data) => {
+		const act = this.props.activities;
+		const rem = this.props.remainder;
+		const total = this.timeSpentToArray(act, rem).reduce((a,b) => a + b);
+		if (total > 24) {
+			return {
+				labels: ['> 24 Hrs'],
+				datasets: [{
+					data: [total],
+					backgroundColor: ['#FF0000'],
+					hoverBackgroundColor: ['#FF0000']
+				}]
+			};
+		} else {
+			return {
+				labels: label,
+				datasets: [{
+					data: data,
+					backgroundColor: this.setColors(),
+					hoverBackgroundColor: this.setColors()
+				}]
+			};
+		}
+	}
 
 	render() {
 		const {activities, remainder} = this.props;

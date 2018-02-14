a42243f8bc023e15511f813cc07b922a14fdee8e
garretteklof <garrett.eklof@gmail.com>
2018-02-14 16:50:56 -0600

    start Navbar component with logo

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom'; //withRouter preserves is-active functionality
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth.js'
+
+export const Navbar = ({ startLogout }) => (
+	<nav className="navbar is-transparent is-primary" role="navigation" aria-label="main navigation">
+		<div className='container'>
+			<div className="navbar-brand">
+				<Link className='navbar-item' to='/'>
+					<img src='/images/carpi-white.svg' alt='carpi' width='112' height='28'/>
+				</Link>
+				<button className="button navbar-burger">
+  				<span></span>
+  				<span></span>
+  				<span></span>
+				</button>
+			</div>
+			<div className="navbar-menu">
+  			<div className="navbar-start">
+  			</div>
+  			<div className="navbar-end">
+  				<div className='navbar-item'>
+  					<a 
+  					className="button is-outlined is-inverted is-primary" 
+  					onClick={startLogout}>Logout
+  					</a>
+  				</div>
+  			</div>
+  		</div>
+		</div>
+	</nav>
+
+
+);
+
+const mapDispatchToProps = (dispatch) => ({
+	startLogout: () => dispatch(startLogout())
+});
+
+export default withRouter(connect(undefined, mapDispatchToProps)(Navbar));

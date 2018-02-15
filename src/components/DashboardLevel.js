4942e4782bad73d4a4c68aadecef976b6b59543f
garretteklof <garrett.eklof@gmail.com>
2018-02-15 15:23:58 -0600

    add DiemCounter to DashboardLevel

--- src/components/DashboardLevel.js
+++ src/components/DashboardLevel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import DiemListDateFilter from './DiemListDateFilter';
 import DiemListActivityFilter from './DiemListActivityFilter';
+import DiemCounter from './DiemCounter';
 
 export const DashboardLevel = () => (
 	<div className='level'>
@@ -10,6 +11,9 @@ export const DashboardLevel = () => (
 		</div>
 		<div className='level-item'>
 			<DiemListActivityFilter />
+		</div>
+		<div className='level-item'>
+			<DiemCounter />
 		</div>
 		<div className='level-item'>
 			<Link className='button is-primary' to='/create'>New Diem</Link>

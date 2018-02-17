c15a93dc2b33c558ebe171c21f162d921fb80039
garretteklof <garrett.eklof@gmail.com>
2018-02-16 18:35:41 -0600

    move styles back to ActivityLineGraph for store

--- src/components/ActivityLineGraph.js
+++ src/components/ActivityLineGraph.js
@@ -40,50 +40,54 @@ export class ActivityLineGraph extends React.Component {
 
   render() {
     return (
-      <Line 
-        data={{
-          labels: this.createDateAxis(this.props.filters),
-          datasets: [
-            {
-              label: this.props.filters.activityGraph,
-              fill: false,
-              lineTension: 0.1,
-              backgroundColor: 'rgba(75,192,192,0.4)',
-              borderColor: 'rgba(75,192,192,1)',
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.0,
-              borderJoinStyle: 'miter',
-              pointBorderColor: 'rgba(75,192,192,1)',
-              pointBackgroundColor: '#fff',
-              pointBorderWidth: 1,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-              pointHoverBorderColor: 'rgba(220,220,220,1)',
-              pointHoverBorderWidth: 2,
-              pointRadius: 1,
-              pointHitRadius: 10,
-              data: this.createDataPoints(this.props.diems, this.props.filters)
-            }
-          ]
-        }}
-        options={{
-          scales: {
-            xAxes: [{
-              type: 'time',
-              time: {
-                unit: 'day',
-                tooltipFormat: 'MMM D, YYYY',
-                displayFormats: {
-                  day: 'MMM D'
+      <div className='section'>
+        <div className='container'>
+          <Line 
+            data={{
+              labels: this.createDateAxis(this.props.filters),
+              datasets: [
+                {
+                  label: this.props.filters.activityGraph,
+                  fill: false,
+                  lineTension: 0.1,
+                  backgroundColor: 'rgba(75,192,192,0.4)',
+                  borderColor: 'rgba(75,192,192,1)',
+                  borderCapStyle: 'butt',
+                  borderDash: [],
+                  borderDashOffset: 0.0,
+                  borderJoinStyle: 'miter',
+                  pointBorderColor: 'rgba(75,192,192,1)',
+                  pointBackgroundColor: '#fff',
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                  pointHoverBorderColor: 'rgba(220,220,220,1)',
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  data: this.createDataPoints(this.props.diems, this.props.filters)
                 }
+              ]
+            }}
+            options={{
+              scales: {
+                xAxes: [{
+                  type: 'time',
+                  time: {
+                    unit: 'day',
+                    tooltipFormat: 'MMM D, YYYY',
+                    displayFormats: {
+                      day: 'MMM D'
+                    }
+                  }
+                }],
+                yAxes: [{
+                }]
               }
-            }],
-            yAxes: [{
-            }]
-          }
-        }}
-      />
+            }}
+          />
+        </div>
+      </div>
     );
   }
 }

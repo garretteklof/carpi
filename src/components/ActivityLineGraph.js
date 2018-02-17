d831084b2c0ec51cccbbfba63d6c0e27ffaaeef0
garretteklof <garrett.eklof@gmail.com>
2018-02-16 18:26:56 -0600

    wrap ActivityLineGraph for easier styling

--- src/components/ActivityLineGraph.js
+++ src/components/ActivityLineGraph.js
@@ -17,13 +17,6 @@ export class ActivityLineGraph extends React.Component {
     return days;
   }
 
-  createHoursAxis = (diems, { activityGraph }) => {
-    const isolateActivity = diems.map(({activities}) => 
-      activities.filter((activity) => activity.name === activityGraph));
-    const flattenIt = [].concat(...isolateActivity);
-    return flattenIt.map(({timeSpent}) => timeSpent);
-  }
-
   createDataPoints = (diems, { activityGraph }) => {
     const coordinates = diems.map(({activities, date}) => {
       const activity = activities.filter((activity) => activity.name === activityGraph);
@@ -47,52 +40,50 @@ export class ActivityLineGraph extends React.Component {
 
   render() {
     return (
-      <div>
-        <Line 
-          data={{
-            labels: this.createDateAxis(this.props.filters),
-            datasets: [
-              {
-                label: this.props.filters.activityGraph,
-                fill: false,
-                lineTension: 0.1,
-                backgroundColor: 'rgba(75,192,192,0.4)',
-                borderColor: 'rgba(75,192,192,1)',
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: 'rgba(75,192,192,1)',
-                pointBackgroundColor: '#fff',
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                pointHoverBorderColor: 'rgba(220,220,220,1)',
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: this.createDataPoints(this.props.diems, this.props.filters)
-              }
-            ]
-          }}
-          options={{
-            scales: {
-              xAxes: [{
-                type: 'time',
-                time: {
-                  unit: 'day',
-                  tooltipFormat: 'MMM D, YYYY',
-                  displayFormats: {
-                    day: 'MMM D'
-                  }
-                }
-              }],
-              yAxes: [{
-              }]
+      <Line 
+        data={{
+          labels: this.createDateAxis(this.props.filters),
+          datasets: [
+            {
+              label: this.props.filters.activityGraph,
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: 'rgba(75,192,192,0.4)',
+              borderColor: 'rgba(75,192,192,1)',
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: 'rgba(75,192,192,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: this.createDataPoints(this.props.diems, this.props.filters)
             }
-          }}
-        />
-      </div>
+          ]
+        }}
+        options={{
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                unit: 'day',
+                tooltipFormat: 'MMM D, YYYY',
+                displayFormats: {
+                  day: 'MMM D'
+                }
+              }
+            }],
+            yAxes: [{
+            }]
+          }
+        }}
+      />
     );
   }
 }
---
+++ src/components/ActivityLineGraphWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ActivityLineGraph from './ActivityLineGraph';
+
+
+export const ActivityLineGraphWrapper = () => (
+	<div className='section'>
+		<div className='container'>
+			<ActivityLineGraph />
+		</div>
+	</div>
+);
+
+export default ActivityLineGraphWrapper;

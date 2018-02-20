e632e3e4057da45e5777ef3164404e2d53c610bb
garretteklof <garrett.eklof@gmail.com>
2018-02-20 12:23:39 -0600

    fix loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Loading = () => (
-	<div className='loader'>
-		<img className='loader__image' src='/images/loader.gif' />
+	<div className='container'>
+		<img className='loading' src='/images/loading.gif' />
 	</div>
 );
 

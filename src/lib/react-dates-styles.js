4be6571322b82e5d99d1306c50958c9e25f2cf12
garretteklof <garrett.eklof@gmail.com>
2018-03-06 21:39:58 -0600

    customize react-dates to adhere better to color scheme

--- src/lib/react-dates-styles.js
+++ src/lib/react-dates-styles.js
@@ -6,8 +6,36 @@ ThemedStyleSheet.registerInterface(aphroditeInterface);
 ThemedStyleSheet.registerTheme({
   reactDates: {
     ...DefaultTheme.reactDates,
+    border: {
+      input: {
+        ...DefaultTheme.reactDates.border.input,
+        borderBottomFocused: `2px solid #1dd3b0`
+      }
+    },
     color: {
-      ...DefaultTheme.reactDates.color
+      ...DefaultTheme.reactDates.color,
+      selectedSpan: {
+        backgroundColor: '#95F9E3',
+        backgroundColor_active: '#1dd3b0',
+        backgroundColor_hover: '#1dd3b0',
+        borderColor: '#1dd3b0',
+        borderColor_active: '#1dd3b0',
+        borderColor_hover: '#1dd3b0',
+        color: '#fff !important',
+        color_active: '#fff !important',
+        color_hover: '#fff !important'
+      },
+      selected: {
+        backgroundColor: '#1dd3b0',
+        backgroundColor_active: '#1dd3b0',
+        backgroundColor_hover: '#1dd3b0',
+        borderColor: '#1dd3b0',
+        borderColor_active: '#1dd3b0',
+        borderColor_hover: '#1dd3b0',
+        color: '#fff !important',
+        color_active: '#fff !important',
+        color_hover: '#fff !important'
+      }
     }
   }
 });
